985ee2381c70fa7c3489ffeaadb032d95c3dce32
taehyeong9210 <skytubu@naver.com>
2023-11-10 14:26:23 +0900

    23년 11월 11일

--- noom(video)/src/public/app.js
+++ noom(video)/src/public/app.js
@@ -77,8 +77,13 @@ const handlecameraBtn = () => {
   }
 };
 
-function handleCameraChange() {
-  getMedia(camerasSelect.value);
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === 'video');
+  }
 }
 
 muteBtn.addEventListener('click', handlebuttonBtn);
@@ -93,17 +98,18 @@ const welcomeForm = welcome.querySelector('form');
 
 call.hidden = true;
 
-async function startMedia() {
+async function initCall() {
   welcome.hidden = true;
   call.hidden = false;
   await getMedia();
   makeConnection();
 }
 
-function handleWelcomeSubmit(e) {
+async function handleWelcomeSubmit(e) {
   e.preventDefault();
   const input = welcomeForm.querySelector('input');
-  socket.emit('join_room', input.value, startMedia);
+  await initCall();
+  socket.emit('join_room', input.value);
   roomName = input.value;
   input.value = '';
 }
@@ -113,19 +119,42 @@ welcomeForm.addEventListener('submit', handleWelcomeSubmit);
 socket.on('welcome', async () => {
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
-  console.log('sent the offer');
+
   socket.emit('offer', offer, roomName);
 });
 
-socket.on('offer', (offer) => console.log(offer));
+socket.on('offer', async (offer) => {
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit('answer', answer, roomName);
+});
 
-// RTC 코드`
+socket.on('answer', (answer) => {
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on('ice', (ice) => {
+  myPeerConnection.addIceCandidate(ice);
+});
+
+// RTC 코드
 
 function makeConnection() {
   myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener('icecandidate', handleIce);
+  myPeerConnection.addEventListener('addstream', handleAddStream);
   console.log(
     myStream
       .getTracks()
       .forEach((track) => myPeerConnection.addTrack(track, myStream)),
   );
+}
+
+function handleIce(data) {
+  socket.emit('ice', data.candidate, roomName);
+}
+function handleAddStream(data) {
+  const peerFace = document.getElementById('peerFace');
+  peerFace.srcObject = data.stream;
 }
