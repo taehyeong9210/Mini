5888c71059ab13e8bfcbeda3a831764375138e1d
taehyeong9210 <skytubu@naver.com>
2023-11-16 14:58:13 +0900

    23년 11월 16일

---
+++ twitter/src/components/PostForm.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  resize: none;
+  &::placeholder {
+    font-size: 16px;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+      Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+      sans-serif;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+
+const AttachFileButton = styled.label`
+  padding: 10px 0px;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+const AttachFileInput = styled.input`
+  display: none;
+`;
+
+const SubmitButton = styled.input`
+  background-color: #1d9bf0;
+  color: white;
+  border: none;
+  padding: 10px 0px;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.9;
+  }
+`;
+
+const PostForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tweets, setTweets] = useState('');
+  const [file, setFile] = useState(null);
+
+  const onChange = (e) => {
+    setTweets(e.target.value);
+  };
+
+  const onFileChange = (e) => {
+    const { files } = e.target;
+    if (files && files.length === 1) {
+      setFile(files[0]);
+    }
+  };
+
+  return (
+    <>
+      <Form>
+        <TextArea
+          rows={5}
+          maxLength={180}
+          onChange={onChange}
+          value={tweets}
+          placeholder="what is Happning?"
+        />
+        <AttachFileButton htmlFor="file">
+          {file ? 'Photo added' : 'Add Photo'}
+        </AttachFileButton>
+        <AttachFileInput
+          onChange={onFileChange}
+          type="file"
+          id="file"
+          accept="image/*"
+        ></AttachFileInput>
+        <SubmitButton
+          type="submit"
+          value={isLoading ? 'Posting...' : 'Post Tweet'}
+        ></SubmitButton>
+      </Form>
+    </>
+  );
+};
+
+export default PostForm;
--- twitter/src/routes/Home.js
+++ twitter/src/routes/Home.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { auth } from '../firebase';
+import PostForm from '../components/PostForm';
+import styled from 'styled-components';
+
+const Wrapper = styled.div``;
 
 const Home = () => {
   const logOut = () => {
     auth.signOut();
   };
-  return <div></div>;
+  return (
+    <Wrapper>
+      <PostForm></PostForm>
+    </Wrapper>
+  );
 };
 
 export default Home;
